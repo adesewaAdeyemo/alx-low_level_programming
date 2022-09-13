b0598d89b78e077811c9168b6f0380a1095cd2f9
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-13 15:27:00 -0700

    asd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		putchar(*c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
