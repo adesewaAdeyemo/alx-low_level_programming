0e58f232634f65b04568188ae067108bdcadbcc8
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-13 17:16:09 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0 success
  */
 int main(void)
 {
 	print_alphabet();
+	_putchar('\n');
 	return (0);
 }
-void print_alphabet(void)
 {
 	char i;
 
 	for(i = 'a'; i <= 'z'; i++)
-		putchar(i);
+		_putchar(i);
 }
