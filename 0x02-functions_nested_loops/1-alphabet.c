b0e3926fa0a69d3d2cfd942b42fe7e769651c3a0
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-13 18:05:48 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,10 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * print_alphabet - check description
+ * Description - To print alphabets
  *
- * Description: A program that prints _putchar,
- * followed by a new line.
- *
- * Return: Always 0 success
+ * Return: void
  */
-
 void print_alphabet(void)
 {
 	char i;
