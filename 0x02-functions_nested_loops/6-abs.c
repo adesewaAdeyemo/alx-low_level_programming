add4bcc2e792961e38f88bfbbb1da8fa427b8888
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-13 18:32:06 -0700

    .

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * @n: n is an integer
+ *
+ * Return: integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
