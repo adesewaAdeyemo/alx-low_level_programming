d75332b2bd809dacca80ed1c13c9406315b0ba3a
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-15 16:59:32 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,10 +6,13 @@
  * Return: void
  */
 void more_numbers(void)
+{
+	int i,j;
+
 	for (i = 0; i < 10; i++)
 		j = 0;
 		if( j <= 14 )
 			_putchar(j);
-		_putchar('\n')
-	_putchar('\n')
+		_putchar('\n');
+	_putchar('\n');
 }
