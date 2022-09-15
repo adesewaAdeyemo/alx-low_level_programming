04199aa79174bd1c37369024b426c871867b8e06
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-15 16:55:31 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers severally
+ *
+ * Return: void
+ */
+void more_numbers(void)
+	for (i = 0; i < 10; i++)
+		j = 0;
+		if( j <= 14 )
+			_putchar(j);
+		_putchar('\n')
+	_putchar('\n')
+}
