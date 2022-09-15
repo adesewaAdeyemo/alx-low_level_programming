822c659a43032b372dffd4eed12a085d9335fd34
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-15 15:37:05 -0700

    .

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#nclude "main.h"
+
+/**
+ * _isdigit - check for a digit (0 through 9)
+ *
+ * @c: integar value
+ *
+ * Return 1 if c is a digit,
+ * 0 if c is not
+ */
+
+int _isdigit(int c)
+{
+	if(c >= 48 && c <= 57)
+	{
+		retrurn(1);
+	}
+	else
+	{
+		return (0);
+	}
+}
