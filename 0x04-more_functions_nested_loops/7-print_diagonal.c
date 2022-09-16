35ddb85af11f102d11a2938bc63125f204707537
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-15 19:18:38 -0700

    s

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,13 +10,13 @@
 
 void print_diagonal(int n)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
 		if (n < 0)
 		{
-			for (j=0, j < n, j++)
+			for (j = 0, j < n, j++)
 			{
 				putchar(' ');
 			}
