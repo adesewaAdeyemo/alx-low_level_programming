8a0a93170100a86077af801030d7272984bcbc56
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-15 20:33:17 -0700

    s

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,18 @@ void print_integer(int m);
 void print_number(int n)
 {
 	if (n == 0)
+	{
 		_putchar('0');
+	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		print_integer(n * -1);
 	}
 	else
+	{
 		print_integer(n);
+	}
 }
 
 /**
@@ -29,8 +33,10 @@ void print_integer(int m)
 	int i = 1000000000;
 
 	for (; i >= 1; i /= 10)
+	{
 		if (m / i != 0)
 		{
 			_putchar((m / i) % 10 + '0');
 		}
+	}
 }
