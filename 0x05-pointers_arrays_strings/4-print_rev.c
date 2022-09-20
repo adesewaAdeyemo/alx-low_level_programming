8fd1448e7c1651b8cbdbe495aafaf3f150144138
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-19 17:40:15 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @s : string parameter to print
+ *
+ * Return: void
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	if (s[i] == '/0')
+	{
+		for (j = i-1; j >= 0; i--)
+		{
+			_putchar(s[j] + 0);
+		}
+		_putchar('\n');
+	}
+}
