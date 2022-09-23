7fc0476588ca9a6f6611fabf6d9d8d1c5e8c6221
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-22 17:29:53 -0700

    s

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
     char s1[98] = "Hello ";
@@ -15,9 +15,10 @@ int main(void)
     printf("%s\n", s1);
     printf("%s", s2);
     ptr = _strncat(s1, s2, 1);
-    printf("%s", s1);
+    printf("%s\n", s1);
     printf("%s", s2);
-    printf("%s", ptr);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
