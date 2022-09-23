b9fb244f63febdbda509f06e0824825dfb5685be
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-22 18:05:35 -0700

    d

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: input
+ * @n: int
+ *
+ * Return (0)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int aux;
+
+	while (i < n--)
+	{
+		aux = a[1];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
+}
