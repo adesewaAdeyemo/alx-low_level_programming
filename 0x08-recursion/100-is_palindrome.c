a2ec53b7f48b4ab1563cdbff67e2081012923c98
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2022-09-28 18:34:45 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,62 @@
 #include "main.h"
 
-int is_divisible(int num, int div);
-int is_prime_number(int n);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 /**
- * is_divisible - Checks if a number is divisible.
- * @num: The number to be checked.
- * @div: The divisor.
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * Return: If the number is divisible - 0.
- *         If the number is not divisible - 1.
+ * Return: The length of the string.
  */
-int is_divisible(int num, int div)
+int find_strlen(char *s)
 {
-	if (num % div == 0)
-		return (0);
+	int len = 0;
 
-	if (div == num / 2)
-		return (1);
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
 
-	return (is_divisible(num, div + 1));
+	return (len);
 }
 
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to be checked.
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
  *
- * Return: If the integer is not prime - 0.
- *         If the number is prime - 1.
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
  */
-int is_prime_number(int n)
+int check_palindrome(char *s, int len, int index)
 {
-	int div = 2;
-
-	if (n <= 1)
-		return (0);
-
-	if (n >= 2 && n <= 3)
+	if (s[index] == s[len / 2])
 		return (1);
 
-	return (is_divisible(n, div));
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
 }
